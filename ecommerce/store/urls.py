d348d2c5b2ad853ee441582ca408a4b0a7a40dcd
Undeadtaker <61665341+Undeadtaker@users.noreply.github.com>
2023-01-06 14:29:14 -0800

    Remove celery test url

--- ecommerce/store/urls.py
+++ ecommerce/store/urls.py
@@ -7,5 +7,4 @@ urlpatterns = \
         path('', views.product_all, name='product_all'),
         path('<slug:slug>/', views.product_single, name='product_single'),
         path('shop/<slug:category_slug>/', views.category_single, name='category_single'),
-        path('test', views.test_celery)
     ]

a3d8a05479810ee676c1bf57e9a24726a29cfcc3
Undeadtaker <filipkasic@gmail.com>
2023-01-11 18:28:50 +0100

    Major Update
    Since discovering django-cookie-cutter, I have decided to move the project I have built thus far into a separate folder.
    
    I will start the implementation of this project again from the ground up. This shouldn't be an issue since most of the integration from the previous version is already implemented here using the django-allauth third party app.
    
    This will gradually devolve from having both local and production environments to just production.

--- ecommerce/account/tasks.py
+++
@@ -1,9 +0,0 @@
-from celery import shared_task
-
-from .models import CustomUser
-
-@shared_task(bind=True)
-def celery_send_mail(request, user_id, subject, message):
-    user = CustomUser.objects.get(pk = user_id)
-    user.email_user(subject=subject, message=message)
-    return None
--- ecommerce/cart/context_processors.py
+++
@@ -1,5 +0,0 @@
-from .basket import Basket
-
-# Returns the store session
-def basket(req):
-    return {'basket': Basket(req)}

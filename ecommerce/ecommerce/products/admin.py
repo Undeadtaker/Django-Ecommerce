cd9cc0376c22eb3d7e8fdbe3dc8dbc87e6acfbce
Undeadtaker <filipkasic@gmail.com>
2023-01-16 17:39:51 +0100

    Implemented inline Model creation in admin area
    Admin and staff can now add Products and their images from just one page.

--- ecommerce/ecommerce/products/admin.py
+++ ecommerce/ecommerce/products/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 
-from .models import Product, Category
+from mptt.admin import MPTTModelAdmin
+
+from .models import Product, Category, ProductImage
 
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
+admin.site.register(Category, MPTTModelAdmin)
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [
+        ProductImageInline,
+    ]
+
